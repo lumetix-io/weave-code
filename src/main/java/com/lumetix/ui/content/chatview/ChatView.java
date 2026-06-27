79556a9f8ed57041416c96374660c9663a1be20d
TJ.MA <tj.ma@tj.ma>
2026-06-27 16:06:38 +0800

    aissistant init

--- src/main/java/com/lumetix/ui/content/chatview/ChatView.java
+++ src/main/java/com/lumetix/ui/content/chatview/ChatView.java
@@ -1,23 +1,48 @@
 package com.lumetix.ui.content.chatview;
 
 import com.lumetix.entity.chat.ChatDetail;
+import com.lumetix.entity.chat.ChatEnum;
+import javafx.collections.ListChangeListener;
+import javafx.concurrent.Worker;
 import javafx.geometry.Insets;
-import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebView;
+import org.commonmark.Extension;
+import org.commonmark.ext.autolink.AutolinkExtension;
+import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.lumetix.entity.BasicConstants.ChatUi.chatList;
-import static com.lumetix.entity.BasicConstants.ChatUi.chatViewRefresh;
 import static com.lumetix.entity.BasicConstants.InPutUi.TEXTAREA_WIDTH;
 import static com.lumetix.ui.content.basicInputview.InputView.newInputViewWithBorder;
 
 public class ChatView {
 
+
+    private static final Parser PARSER;
+    private static final HtmlRenderer RENDERER;
     private static final double CHAT_VIEW_WIDTH = TEXTAREA_WIDTH;
 
+    static {
+        List<Extension> extensions = Arrays.asList(
+                TablesExtension.create(),
+                StrikethroughExtension.create(),
+                AutolinkExtension.create()
+        );
+        PARSER = Parser.builder().extensions(extensions).build();
+        RENDERER = HtmlRenderer.builder().extensions(extensions).build();
+    }
 
     public static BorderPane newChatViewWrapper() {
         StackPane input = newInputViewWithBorder();
@@ -30,27 +55,66 @@ public class ChatView {
         return chatView;
     }
 
-    public static ListView<ChatDetail> newChatView() {
-        ListView<ChatDetail> listView = new ListView<>();
-        listView.itemsProperty().bindBidirectional(chatList);
-        listView.setCellFactory(param -> new ChatCell());
 
-        listView.setBorder(Border.EMPTY);
-        listView.getStylesheets().add("data:text/css," +
-                ".scroll-bar:vertical { -fx-opacity: 0; -fx-pref-width: 0; }"
-        );
-        chatList.addListener((_, _, newValue) ->
-        {
+    public static ScrollPane newChatView() {
+        ScrollPane scrollPane = new ScrollPane();
+        VBox vBox = new VBox();
+        chatList.addListener((_, _, newValue) -> {
             if (newValue.isEmpty()) {
-                return;
+                vBox.getChildren().clear();
             }
-            ChatDetail last = newValue.getLast();
-            if (Objects.isNull(last)) {
-                return;
-            }
-            listView.scrollTo(last);
         });
-        chatViewRefresh.addListener((observable, oldValue, newValue) -> listView.refresh());
-        return listView;
+        chatList.addListener((ListChangeListener<ChatDetail>) changelist -> {
+            for (ChatDetail detail : changelist.getList()) {
+                String type = detail.getType();
+                if (type.equals(ChatEnum.USER.name())) {
+                    TextArea textArea = new TextArea();
+                    textArea.setText(detail.getContent());
+                    textArea.setEditable(false);
+                    textArea.setBorder(Border.EMPTY);
+                    vBox.getChildren().add(textArea);
+                } else {
+                    Node document = PARSER.parse(detail.getContent());
+
+                    // 5. 将 AST 渲染为 HTML 字符串
+                    String html = RENDERER.render(document);
+
+                    WebView webView = new WebView();
+                    //    webView.setPrefWidth(this.getWidth());
+                    webView.getEngine().loadContent(html);
+                    webView.getEngine().getLoadWorker().stateProperty().addListener((obs, o, n) -> {
+                        if (n == Worker.State.SUCCEEDED)
+                            webView.setPrefHeight(((Number) webView.getEngine()
+                                    .executeScript("document.body.scrollHeight")).doubleValue() + 10);
+                    });
+                    vBox.getChildren().add(webView);
+                }
+            }
+        });
+        scrollPane.setContent(vBox);
+        return scrollPane;
     }
+//    public static ListView<ChatDetail> newChatView() {
+//        ListView<ChatDetail> listView = new ListView<>();
+//        listView.itemsProperty().bindBidirectional(chatList);
+//        listView.setCellFactory(param -> new ChatCell());
+//
+//        listView.setBorder(Border.EMPTY);
+//        listView.getStylesheets().add("data:text/css," +
+//                ".scroll-bar:vertical { -fx-opacity: 0; -fx-pref-width: 0; }"
+//        );
+//        chatList.addListener((_, _, newValue) ->
+//        {
+//            if (newValue.isEmpty()) {
+//                return;
+//            }
+//            ChatDetail last = newValue.getLast();
+//            if (Objects.isNull(last)) {
+//                return;
+//            }
+//            listView.scrollTo(last);
+//        });
+//        chatViewRefresh.addListener((observable, oldValue, newValue) -> listView.refresh());
+//        return listView;
+//    }
 }
